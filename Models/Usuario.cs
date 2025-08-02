291471d034dbd2b386168e3447eef0312f38fcd5
jenn487 <jenniferdocuments07@gmail.com>
2025-08-01 22:42:21 -0400

    Validar los campos del modelo Usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace APP_CRUD.Models;
 
 public partial class Usuario
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [StringLength(50)]
     public string? Nombre { get; set; }
-
+  
     public DateOnly? Fecha { get; set; }
 
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    [DataType(DataType.Password)]
     public string? Contrasenia { get; set; }
 }
