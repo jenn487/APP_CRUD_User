fd0e483eef3bb8023186fd3c4b816fe7981265e5
jenn487 <jenniferdocuments07@gmail.com>
2025-08-01 22:06:18 -0400

    Proyecto CRUD base

---
+++ Models/AppCrudContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APP_CRUD.Models;
+
+public partial class AppCrudContext : DbContext
+{
+    public AppCrudContext()
+    {
+    }
+
+    public AppCrudContext(DbContextOptions<AppCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+               if (!optionsBuilder.IsConfigured)
+        {
+ //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+  //      => optionsBuilder.UseSqlServer("Data Source=DESKTOP-DV2CHUO\\SQLEXPRESS; Initial Catalog=APP_CRUD; Integrated Security=True; TrustServerCertificate=True;");
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC07CF7EB032");
+
+            entity.Property(e => e.Contrasenia)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APP_CRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public string? Contrasenia { get; set; }
+}
